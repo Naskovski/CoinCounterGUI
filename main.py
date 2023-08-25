e8f224977b358b28b8818ab07e0d6f74b9e8f8d9
Naskovski <filip.naskovski.hk@gmail.com>
2023-08-25 19:17:50 +0200

    First UI draft

--- main.py
+++ main.py
@@ -1,17 +1,41 @@
-# This is a sample Python script.
+import tkinter
+from tkinter import *
+from tkinter import ttk
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 #TEST3
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('TEST 2')
-    print_hi('TEST 3')
+    # style vars
+    bgcolor = "#fcfbf4"
+    border_color = "#aa0033"
+    #style = tkinter.ttk.Style()
+    #style.configure('bt_style', foreground=bgcolor, font=("Helvetica", 12), border=border_color)
+
+    #app
+    top = tkinter.Tk()
+    top.title("CoinCounter")
+    top.configure(bg=bgcolor)
+
+    logo = tkinter.PhotoImage(file="./logo_small.png")
+    logo_lbl = tkinter.ttk.Label(top, image=logo, background=bgcolor)
+    logo_lbl.pack(side="top", padx=50, pady=20)
+
+    heading = tkinter.ttk.Label(text="CoinCounter", font=("Helvetica", 25), padding=10, background=bgcolor)
+    heading.pack()
+
+    btn_Open = tkinter.ttk.Button(text="Load Image")
+    btn_Open.pack()
+
+    btn_Close = tkinter.ttk.Button(text="Close")
+    btn_Close.pack()
+
+
+
+
+
+
+
+    top.mainloop()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
